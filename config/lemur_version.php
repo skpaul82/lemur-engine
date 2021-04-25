773c267e72c5140cb22733f59f5e8c3b6595eb69
elizaibeth <seperreau@gmail.com>
2021-04-25 19:59:21 +0100

    bumping the version to get ready for the release

--- config/lemur_version.php
+++ config/lemur_version.php
@@ -8,7 +8,11 @@ return [
     |
     */
     'portal' =>
-        ['id' => '0.4.0'],
+        ['id' => '1.0.0'],
     'bot' =>
-        ['id' => '0.3.0'],
+        ['id' => '1.0.0'],
+    'release' => [
+        'name' => 'Morton\'s Razor',
+        'description' => 'This release is dedicated to my friend and Program O dev - Dave Morton. We always wanted to rewrite Program O. It took a global pandemic for me to find the time to actually be able do it. I wish you were here to see this day - this one is for you mr!'],
+
 ];
